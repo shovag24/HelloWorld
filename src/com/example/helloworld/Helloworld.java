8e6628312dba18988b19d20d4323f224811d214e
shovag24 <shovag24@gmail.comgit config --global user.email shovag24@gmail.com>
2021-07-11 23:05:53 -0230

    Added in line 6

--- src/com/example/helloworld/Helloworld.java
+++ src/com/example/helloworld/Helloworld.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 public class Helloworld {
     public static void main(String[] args) {
 
-        System.out.println("hello world intellig");
+        System.out.println("hello world first intellig");
     }
 }
 
